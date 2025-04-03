920c05487ee0795f992b7af0183f96eef513b3b0
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-04-03 10:34:13 +0200

    methods done

--- db.py
+++ db.py
@@ -15,8 +15,22 @@ class Database:
        self.cur.execute("SELECT * FROM Books")
        rows = self.cur.fetchall()
        return rows
+
    def insert_book(self, bookID,title, author , ISBN, genre, availability_status):
        self.cur.execute("INSERT INTO Book VALUES (NULL, ?, ?, ?, ?, ?)", (bookID, title, author, ISBN,genre, availability_status))
        self.conn.commit()
+
+   def delete_book(self, bookID):
+       self.cur.execute("DELETE FROM Book WHERE bookID=?", (bookID,))
+       self.conn.commit()
+
+   def  update_book(self,  bookID,title, author , ISBN, genre, availability_status):
+       self.cur.execute("UPDATE Book SET title=?, author=?, ISBN=?, genre=? WHERE id=?)" , ( title, author,ISBN, genre, availability_status, bookID))
+       self.conn.commit()
+
+   def __del__(self):
+       self.conn.close()
+db = Database("LibraryDB.db")
+
 
 
