df8b664a14abe46ad2bdfbddfc77eb2c05f9345a
Sphiwe2004 <601211@student.belgiumcampus.ac.za>
2025-04-03 11:27:33 +0200

    added select, insert, update, delete functions for members

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ class Database:
        self.cur.execute(
            "CREATE TABLE IF NOT EXISTS Book( title INTEGER PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
        self.cur.execute(
-           "CREATE TABLE IF NOT EXIST Member( title INTEGER PRIMARY KEY, Name text, MembershipID text, Contact text, MembershipType text CHECK(MembershipType IN ('regular', 'Premium') "
+           "CREATE TABLE IF NOT EXIST MembershipID AUTOINCREMENT INTEGER PRIMARY KEY, Name text, Contact text, MembershipType text CHECK(MembershipType IN ('regular', 'Premium') "
        )
        self.conn.commit()
 
@@ -17,14 +17,14 @@ def select_members(self):
     rows = self.cur.fetchall()
     return rows
 
-def insert_members(self, MembershipID):
-    self.cur.execute("INSERT INTO Members VALUES (MembershipID ?)", (MembershipID))
+def insert_members(self, MembershipID, Name, Contact, MembershipType):
+    self.cur.execute("INSERT INTO Members VALUES (NULL, ?, ?, ?)", (MembershipID, Name, Contact, MembershipType))
     self.conn.commit()
 
 def update_members(self, MembershipID, Name, Contact, MembershipType):
-    self.cur.execute("UPDATE Members VALUES (?)", (MembershipID, Name, Contact, MembershipType))
+    self.cur.execute("UPDATE Members SET MembershipID=?, Name=?, Contact=?, MembershipType=?, WHERE MembershipID=! )", (MembershipID, Name, Contact, MembershipType))
     self.conn.commit()
 
 def delete_members(self, MembershipID):
-    self.cur.execute("DELETE FROM Members VALUES (MembershipID?)", (MembershipID ))
+    self.cur.execute("DELETE FROM Members VALUES (MembershipID?)", (MembershipID, ))
     self.conn.commit()
