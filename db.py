a8c7777ab86c7ab34252ff51c7ce02cf299cc7e6
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-04-03 10:14:08 +0200

    insert book method

--- db.py
+++ db.py
@@ -5,15 +5,18 @@ class Database:
        self.conn = sqlite3.connect(db)
        self.cur = self.conn.cursor()
        self.cur.execute(
-           "CREATE TABLE IF NOT EXISTS Book( title INTEGER PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
+           "CREATE TABLE IF NOT EXISTS Book( BookID INTEGER AUTOINCREMENT PRIMARY KEY, title text, author text, ISBN text, genre text, Avaliability_Status text)")
        self.cur.execute(
            "CREATE TABLE IF NOT EXIST Member( title INTEGER PRIMARY KEY, Name text, MembershipID text, Contact text, MembershipType text CHECK(MembershipType IN ('regular', 'Premium') "
        )
        self.conn.commit()
 
    def fetch_book(self):
-       self.cur.execute("SELECT* FROM Books")
+       self.cur.execute("SELECT * FROM Books")
        rows = self.cur.fetchall()
        return rows
+   def insert_book(self, bookID,title, author , ISBN, genre, availability_status):
+       self.cur.execute("INSERT INTO Book VALUES (NULL, ?, ?, ?, ?, ?)", (bookID, title, author, availability_status))
+       self.conn.commit()
 
 
