8d51e3b812ca3e0b39b7946abecfb008a30fb148
Saziso Buthelezi <sazisoshenge1@gmail.com>
2025-04-03 09:33:56 +0200

    books table created

--- db.py
+++ db.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+class Database:
+   def __init__(self, db):
+       self.conn = sqlite3.connect(db)
+       self.cur = self.conn.cursor()
+       self.cur.execute(
+           "CREATE TABLE IF NOT EXISTS Book( title INTEGER PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
+       self.conn.commit()
