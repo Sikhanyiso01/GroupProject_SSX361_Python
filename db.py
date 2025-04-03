57b58d5e604d34211262c91ee34f2d7b4bf28cac
Sphiwe2004 <601211@student.belgiumcampus.ac.za>
2025-04-03 09:47:20 +0200

    Added a table for member

--- db.py
+++ db.py
@@ -1,9 +1,22 @@
 import sqlite3
 
+def create_database():
+    conn = sqlite3.connect("library.db")
+    conn.close()
+
+def create_tables():
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+
 class Database:
    def __init__(self, db):
        self.conn = sqlite3.connect(db)
        self.cur = self.conn.cursor()
        self.cur.execute(
            "CREATE TABLE IF NOT EXISTS Book( title INTEGER PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
+       self.cur.execute(
+           "CREATE TABLE IF NOT EXIST Member( title INTEGER PRIMARY KEY, Name text, MembershipID text, Contact text, MembershipType text CHECK(MembershipType IN ('regular', 'Premium') "
+       )
        self.conn.commit()
+
+
