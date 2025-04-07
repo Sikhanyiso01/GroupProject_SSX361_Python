b2c12f3640b9302c44a4fd1cbad8929cc26e0f33
Sphiwe2004 <601211@student.belgiumcampus.ac.za>
2025-04-07 09:08:19 +0200

    created a new branch

--- db.py
+++ db.py
@@ -28,22 +28,24 @@ class Database:
        self.cur.execute("UPDATE Book SET title=?, author=?, ISBN=?, genre=? WHERE id=?)" , ( title, author,ISBN, genre, availability_status, bookID))
        self.conn.commit()
 
-def select_members(self):
-    self.cur.execute("SELECT * FROM Members")
-    rows = self.cur.fetchall()
-    return rows
+   def select_members(self):
+       self.cur.execute("SELECT * FROM Members")
+       rows = self.cur.fetchall()
+       return rows
 
-def insert_members(self, MembershipID, Name, Contact, MembershipType):
-    self.cur.execute("INSERT INTO Members VALUES (NULL, ?, ?, ?)", (MembershipID, Name, Contact, MembershipType))
-    self.conn.commit()
+   def insert_members(self, membershipid, name, contact, membershiptype):
+       self.cur.execute("INSERT INTO Members VALUES (NULL, ?, ?, ?)", (membershipid, name, contact, membershiptype))
+       self.conn.commit()
 
-def update_members(self, MembershipID, Name, Contact, MembershipType):
-    self.cur.execute("UPDATE Members SET MembershipID=?, Name=?, Contact=?, MembershipType=?, WHERE MembershipID=! )", (MembershipID, Name, Contact, MembershipType))
-    self.conn.commit()
+   def update_members(self, membershipid, name, contact, membershiptype):
+       self.cur.execute(
+           "UPDATE Members SET MembershipID=?, Name=?, Contact=?, MembershipType=?, WHERE MembershipID=? )",
+           (membershipid, name, contact, membershiptype))
+       self.conn.commit()
 
-def delete_members(self, MembershipID):
-    self.cur.execute("DELETE FROM Members VALUES (MembershipID?)", (MembershipID, ))
-    self.conn.commit()
+   def delete_members(self, membershipid):
+       self.cur.execute("DELETE FROM Members VALUES (MembershipID=?)", (membershipid,))
+       self.conn.commit()
 
 def __del__(self):
     self.conn.close()
