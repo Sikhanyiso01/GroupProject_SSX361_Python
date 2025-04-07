3a43b91c8ad5665d462e3c8d1ec1f8de0e59b2cc
Sphiwe2004 <601211@student.belgiumcampus.ac.za>
2025-04-07 09:35:05 +0200

    created a new branch and made changes to my methods

--- db.py
+++ db.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 class Database:
-   def __init__(self, db):
+   def _init_(self, db):
        self.conn = sqlite3.connect(db)
        self.cur = self.conn.cursor()
        self.cur.execute(
-           "CREATE TABLE IF NOT EXISTS Book( title TEXT PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
+           "CREATE TABLE IF NOT EXISTS Book( title Text PRIMARY KEY, author text, ISBN text, genre text, Avaliability_Status text)")
        self.cur.execute(
-           "CREATE TABLE IF NOT EXIST MembershipID AUTOINCREMENT INTEGER PRIMARY KEY, Name text, Contact text, MembershipType text CHECK(MembershipType IN ('regular', 'Premium') "
+           "CREATE TABLE IF NOT EXIST membershipid AUTOINCREMENT INTEGER PRIMARY KEY, name text, Contact text, membershiptype text"
        )
        self.conn.commit()
 
@@ -28,25 +28,23 @@ class Database:
        self.cur.execute("UPDATE Book SET title=?, author=?, ISBN=?, genre=? WHERE id=?)" , ( title, author,ISBN, genre, availability_status, bookID))
        self.conn.commit()
 
-   def select_members(self):
-       self.cur.execute("SELECT * FROM Members")
-       rows = self.cur.fetchall()
-       return rows
+def select_members(self):
+    self.cur.execute("SELECT * FROM Members")
+    rows = self.cur.fetchall()
+    return rows
 
-   def insert_members(self, membershipid, name, contact, membershiptype):
-       self.cur.execute("INSERT INTO Members VALUES (NULL, ?, ?, ?)", (membershipid, name, contact, membershiptype))
-       self.conn.commit()
+def insert_members(self, membershipid, name, contact, membershiptype):
+    self.cur.execute("INSERT INTO Members VALUES (NULL, ?, ?, ?)", (membershipid, name, contact, membershiptype))
+    self.conn.commit()
 
-   def update_members(self, membershipid, name, contact, membershiptype):
-       self.cur.execute(
-           "UPDATE Members SET MembershipID=?, Name=?, Contact=?, MembershipType=?, WHERE MembershipID=? )",
-           (membershipid, name, contact, membershiptype))
-       self.conn.commit()
+def update_members(self, membershipid, name, contact, membershiptype):
+    self.cur.execute("UPDATE Members SET MembershipID=?, Name=?, Contact=?, MembershipType=?, WHERE MembershipID=! )",( membershipid, name, contact, membershiptype))
+    self.conn.commit()
 
-   def delete_members(self, membershipid):
-       self.cur.execute("DELETE FROM Members VALUES (MembershipID=?)", (membershipid,))
-       self.conn.commit()
+def delete_members(self, membershipid):
+    self.cur.execute("DELETE FROM Members VALUES (MembershipID?)", (membershipid, ))
+    self.conn.commit()
 
-def __del__(self):
+def _del_(self):
     self.conn.close()
 db = Database("LibraryDB.db")
